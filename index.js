ad2577b9eb80b05fe51f4a3d00bb5c0347a3fe97
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-16 10:38:29 +1200

    Set up initial files with hello world

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 3000;
+var pg = require('pg');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});

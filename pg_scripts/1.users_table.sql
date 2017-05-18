c96b1803c4b8c586705623efed8b84a470c1f6a9
Slade <Slademtbutler@gmail.com>
2017-05-19 11:43:50 +1200

    sql scripts folder added with users table script

---
+++ pg_scripts/1.users_table.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    username character varying(255) NOT NULL,
+    password character varying(255) NOT NULL
+);

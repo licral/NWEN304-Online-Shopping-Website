0f9f2bcfb19f7f357b0ef98beea069c17076cf0a
Hector Zhao <hector.zhao@powershop.co.nz>
2017-05-22 19:15:22 +1200

    update the database population script

--- pg_scripts/2.db_prepare.sql
+++ pg_scripts/2.db_prepare.sql
@@ -12,11 +12,16 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username CHARACTER VARYING(255) NOT NULL,
     password CHARACTER VARYING(255) NOT NULL,  -- not sure if we need to store their password if we use OATH
-    email CHARACTER VARYING(255) NOT NULL,
+    email CHARACTER VARYING(255) UNIQUE NOT NULL,
+    first_name CHARACTER VARYING(255),
+    last_name CHARACTER VARYING(255),
+    address CHARACTER VARYING(255),
+    contact_no CHARACTER VARYING(255),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     last_visit_on TIMESTAMP,
 
-    CONSTRAINT emailFormatCheck CHECK (email ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
+    CONSTRAINT emailFormatCheck CHECK (email ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+    CONSTRAINT contactFormatCheck CHECK (contact_no ~ '^(\+64|0)\d{9}$')
 );
 
 CREATE TABLE IF NOT EXISTS artists (
@@ -29,9 +34,10 @@ CREATE TABLE IF NOT EXISTS albums (
     title CHARACTER VARYING(255) NOT NULL,
     description TEXT,
     released_on DATE,
+    genre CHARACTER VARYING(255),
+    image BYTEA,
     is_compilation BOOLEAN NOT NULL,
     price NUMERIC(10,2) NOT NULL,
-    -- image_path CHARACTER VARYING(255),
     artist_id INTEGER REFERENCES artists (id)
 );
 
@@ -43,6 +49,20 @@ CREATE TABLE IF NOT EXISTS songs (
     album_id INTEGER REFERENCES albums (id)
 );
 
+CREATE TABLE IF NOT EXISTS orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS order_details (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders (id),
+    album_id INTEGER REFERENCES albums (id),
+    quantity INTEGER NOT NULL,
+
+    CONSTRAINT quantityCheck CHECK (quantity > 0)
+);
+
 
 /***********************
   Populating test data
@@ -51,11 +71,11 @@ CREATE TABLE IF NOT EXISTS songs (
 INSERT INTO admins (username, password) VALUES
     ('admin', 'admin');
 
-INSERT INTO users (username, password, email, created_at) VALUES
-    ('123', '123', 'abc@abc.com', default),
-    ('Slade', 'slade', 'slade@slade.com', default),
-    ('Bonnie', 'bonnie', 'bonnie@bonnie.com', default),
-    ('Hector', 'hector', 'hector@hector.com', default);
+INSERT INTO users (username, password, email, first_name, last_name, address, contact_no, created_at) VALUES
+    ('123', '123', 'abc@abc.com', '123', '123', 'test_address', '0212345678', default),
+    ('Slade', 'slade', 'slade@slade.com', 'Slade', 'Butler', 'Slade''s home address', '0222222222', default),
+    ('Bonnie', 'bonnie', 'bonnie@bonnie.com', 'Bonnie', 'Liao', 'Bonnie''s home address', '+64211111111', default),
+    ('Hector', 'hector', 'hector@hector.com', 'Hector', 'Zhao', 'Hector''s address', '+64211603955', default);
 
 INSERT INTO artists (artist_name) VALUES
     ('Various Artist'),
@@ -69,3 +89,10 @@ INSERT INTO albums (title, released_on, is_compilation, price, artist_id) VALUES
 INSERT INTO songs (title, track_no, artist_id, album_id) VALUES
     ('Superstar', 3, (SELECT id FROM artists WHERE artist_name = 'Sonic Youth'), (SELECT id FROM albums WHERE title = 'If I Were a Carpenter')),
     ('Looped', 3, (SELECT id FROM artists WHERE artist_name = 'Kiasmos'), (SELECT id FROM albums WHERE title = 'Kiasmos'));
+
+INSERT INTO orders (user_id) VALUES
+    ((SELECT id FROM users WHERE username = 'Hector'));
+
+INSERT INTO order_details (order_id, album_id, quantity) VALUES
+    (1, (SELECT id FROM albums WHERE title = 'If I Were a Carpenter'), 5),
+    (1, (SELECT id FROM albums WHERE title = 'Kiasmos'), 5);

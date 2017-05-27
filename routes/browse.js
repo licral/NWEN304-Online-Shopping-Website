1d59d5d38e4242dd6a2e144bdd34c902422e7f95
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-27 14:56:15 +1200

    Added hover effect to browse items

--- routes/browse.js
+++ routes/browse.js
@@ -14,7 +14,7 @@ module.exports = function (app, pool) {
 
         pool.connect()
             .then(client => {
-                let sql = "SELECT * FROM albums;";
+                let sql = "SELECT id, title, artist_id, price FROM albums;";
 
                 client.query(sql)
                     .then(result => {

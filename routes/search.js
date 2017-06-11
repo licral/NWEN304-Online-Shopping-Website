a5b02eb6df72fea57da2d56bf213638a075ed6fa
Hector Zhao <hector.zhao@powershop.co.nz>
2017-06-11 19:56:44 +1200

    remove advanced search

--- routes/search.js
+++ routes/search.js
@@ -2,60 +2,34 @@ module.exports = function (app, pool) {
     "use strict";
 
     app.post('/search', function (request, respond) {
-        let renderData = {
-            title: 'Browse All Vinyls',
+        let pageData = {
+            title: 'Search Result',
             description: "Browse all vinyl records",
             results: {
                 albums: [],
                 artists: [],
-                songs: [],
-                advanceSearch: []
+                songs: []
             }
         };
 
-        let data = request.body;
+        let sqls = [
+            "SELECT * FROM albums WHERE title = $1",
+            "SELECT * FROM artists WHERE artist_name = $1",
+            "SELECT * FROM songs WHERE title = $1"
+        ];
 
-        if (data.keyword) {  // search for keyword
-            let keyword = data.keyword;
+        Promise.all(sqls.map(sql => {
+            return pool.query(sql, [request.body.keyword]);
+        })).then((arrayOfResult) => {
+            pageData.results.albums.push(...arrayOfResult[0].rows);
+            pageData.results.artists.push(...arrayOfResult[1].rows);
+            pageData.results.songs.push(...arrayOfResult[2].rows);
 
-            let sqls = [
-                "SELECT * FROM albums WHERE title = $1",
-                "SELECT * FROM artists WHERE artist_name = $1",
-                "SELECT * FROM songs WHERE title = $1"
-            ];
-
-            Promise.all(sqls.map(sql => {
-                return pool.query(sql, [keyword]);
-            })).then((arrayOfResult) => {
-                renderData.results.albums.push(...arrayOfResult[0].rows);
-                renderData.results.artists.push(...arrayOfResult[1].rows);
-                renderData.results.songs.push(...arrayOfResult[2].rows);
-
-                respond.render('search_results', renderData);
-            });
-
-        } else { // advanced search
-
-            /*
-             * TODO: need more logic for advanced search.
-             *
-             * for [artist, album, song]
-             * if two or three of them are present, use joined tables
-             * if only one present, search that table only
-             *
-             * also need a js script to check if at least one field has words in it.
-             *
-             */
-
-
-            let sql = "SELECT * FROM albums INNER JOIN artists ON albums.artist_id = artists.id INNER JOIN songs ON songs.artist_id = artists.id WHERE artists.artist_name = $1 AND albums.title = $2 AND songs.title = $3";
-
-            pool.query(sql, [data.artist, data.album, data.song]).then(result => {
-                renderData.results.advanceSearch.push(...result.rows);
-                respond.render('search_results', renderData);
-            }).catch(e => {
-                console.error('[ERROR] Query error', e.message, e.stack);
-            });
-        }
+            respond.render('search_results', pageData);
+        }).catch(error => {
+            pageData.error = "Database error occurred, please refresh or contact hectorcaesar@hotmail.com.";
+            respond.render('search_results', pageData);
+            console.error('[ERROR] Query error', error.message, error.stack);
+        });
     });
 };

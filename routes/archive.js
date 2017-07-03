e25690484a2abe554eb475b108580a80444a1c04
licral <kurousagiwasugoi@gmail.com>
2017-07-03 21:33:14 +1200

    Invalid files are handled when viewing an archived file. Also restricts access to admin only

--- routes/archive.js
+++ routes/archive.js
@@ -2,7 +2,7 @@ module.exports = function (app, pool) {
     const fs = require("fs");
     const path = require('path');
 
-    app.post("/archive", function (request, response) {
+    app.post("/archive", isAdmin, function (request, response) {
         // request.body example: { '16': 'on', '17': 'on', '18': 'on' }
         let arrayOfIds = Object.keys(request.body);
 
@@ -28,11 +28,12 @@ module.exports = function (app, pool) {
         }
     });
 
-    app.get("/order_archives", function (request, response) {
+    app.get("/order_archives", isAdmin, function (request, response) {
         let dirName = path.join(__dirname, '../archives/orders/');
         let pageData = {
             title: 'View order archives',
-            description: "View order archives"
+            description: "View order archives",
+            error: request.flash('error')
         };
 
         let files = fs.readdirSync(dirName);
@@ -45,11 +46,32 @@ module.exports = function (app, pool) {
         }).render('order_archives', pageData);
     });
 
-    app.get("/order_archives/:file", function (request, response) {
+    app.get("/order_archives/:file", isAdmin, function (request, response) {
         let fileName = request.params.file;
         let dirName = path.join(__dirname, '../archives/orders/');
         let fullPath = dirName + fileName;
 
-        response.download(fullPath);
+        response.download(fullPath, function(err){
+            console.error(err);
+            request.flash('error', 'Sorry this file cannot be found');
+            response.redirect('/order_archives');
+        });
     });
 };
+
+function isAdmin(request, response, next) {
+
+    if (request.user) {
+        // if user is authenticated in the session, carry on
+        if (request.user.is_admin) {
+            return next();
+        } else {
+            // if they aren't redirect them to the home page
+            request.flash('error', 'Sorry you do not have permissions to access this content');
+            response.redirect('/');
+        }
+    } else {
+        request.flash('error', 'You must be logged in to access this content');
+        response.redirect('/login');
+    }
+}
